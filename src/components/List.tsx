8f1b61458b9bae013c4183585e370d445a71e550
itgk.j0x <itgk.j0x@gmail.com>
2025-05-19 13:50:41 +0900

    base-design

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,19 +1,14 @@
-import { Card, Typography, CardContent, } from '@mui/material';
+import { Card, Typography, CardContent, CardHeader, } from '@mui/material';
 
 function List() {
   return (
     <>
-    <Card sx={{m:2,px:7,py:0}}>
-      <CardContent>
-        <Typography gutterBottom sx={{
-          textAlign:'right',
-          fontSize: 30 
-          }}>
-          Title
-        </Typography>
+    <Card sx={{m:2,px:4,py:2}}>
+      <CardHeader title="タイトル" sx={{p:0}}/> 
+      <CardContent sx={{p:0}}>
         <Typography variant="body2" sx={{
           color: 'text.secondary',
-          fontSize: 15
+          fontSize: 15,
           }}>
           やること
         </Typography>
