7bea61cce1289c8175a38dd0378cd1c362488406
itgk.j0x <itgk.j0x@gmail.com>
2025-05-20 17:45:40 +0900

    footerつき

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,12 @@
-<<<<<<< HEAD
 import './App.css'
 import ListBox from './components/ListBox'
-=======
-import "./App.css";
 import Footer from "./Footer";
->>>>>>> ea4f768cb3f641575c0fefc2428e0f5cc06197c7
 
 function App() {
   return (
     <>
-<<<<<<< HEAD
     <ListBox />
-=======
-      <Footer />
->>>>>>> ea4f768cb3f641575c0fefc2428e0f5cc06197c7
+    <Footer />
     </>
   );
 }
