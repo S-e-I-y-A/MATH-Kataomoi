ec34425050fae129c18d46f3f89e7a579480a6c9
itgk.j0x <itgk.j0x@gmail.com>
2025-05-23 17:33:56 +0900

    update

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,7 @@ import React, { useState } from "react";
 import Header from "./components/Header";
 import ListBox from "./components/ListBox";
 import Footer from "./Footer";
-import Add from "./components/Add"
-import Navi from "./components/Navi"
+import Add from "./components/Add";
 
 
 function App() {
